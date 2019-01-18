0117de2b36a105f9571482746bb9c4b87cad6ef0
Shalindra Shrestha <shalindra.shrestha@gmail.com>
2019-01-18 15:16:34 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,185 @@
+let rawData = [{"measure_id":1,"measure_name":"Deaths","location_id":108,"location_name":"Belize","sex_id":1,"sex_name":"Male","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.11106562231873124,"upper":0.13431723563669823,"lower":0.09007127002844932},{"measure_id":1,"measure_name":"Deaths","location_id":108,"location_name":"Belize","sex_id":2,"sex_name":"Female","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.05271716391781033,"upper":0.06220008217075092,"lower":0.04340834686876427},{"measure_id":1,"measure_name":"Deaths","location_id":108,"location_name":"Belize","sex_id":3,"sex_name":"Both","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.08224296715130404,"upper":0.09477680114265163,"lower":0.06907802870534893},{"measure_id":1,"measure_name":"Deaths","location_id":7,"location_name":"North Korea","sex_id":1,"sex_name":"Male","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.6270246325440587,"upper":0.9126391324555926,"lower":0.3635222695471738},{"measure_id":1,"measure_name":"Deaths","location_id":7,"location_name":"North Korea","sex_id":2,"sex_name":"Female","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.24920984228447135,"upper":0.34808327652591825,"lower":0.1609982681985628},{"measure_id":1,"measure_name":"Deaths","location_id":7,"location_name":"North Korea","sex_id":3,"sex_name":"Both","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.42654352813332264,"upper":0.57986181764851,"lower":0.2782027921753279},{"measure_id":1,"measure_name":"Deaths","location_id":133,"location_name":"Venezuela","sex_id":1,"sex_name":"Male","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.2776045032544224,"upper":0.3262845068504389,"lower":0.23314606492926562},{"measure_id":1,"measure_name":"Deaths","location_id":133,"location_name":"Venezuela","sex_id":2,"sex_name":"Female","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.11729261626837606,"upper":0.1330069659923833,"lower":0.0985606043898976},{"measure_id":1,"measure_name":"Deaths","location_id":133,"location_name":"Venezuela","sex_id":3,"sex_name":"Both","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.1971727982268931,"upper":0.2226324855146187,"lower":0.1715007757025063},{"measure_id":1,"measure_name":"Deaths","location_id":184,"location_name":"Mozambique","sex_id":1,"sex_name":"Male","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.38514721462675616,"upper":0.6120791835221543,"lower":0.1571092052533996},{"measure_id":1,"measure_name":"Deaths","location_id":184,"location_name":"Mozambique","sex_id":2,"sex_name":"Female","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.04842873015480857,"upper":0.0675592865752726,"lower":0.026521832806552847},{"measure_id":1,"measure_name":"Deaths","location_id":184,"location_name":"Mozambique","sex_id":3,"sex_name":"Both","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.21108223579819024,"upper":0.3239346986331365,"lower":0.09537985034245615},{"measure_id":1,"measure_name":"Deaths","location_id":58,"location_name":"Estonia","sex_id":1,"sex_name":"Male","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":1.8253099105854313,"upper":2.0915683560034184,"lower":1.5721028594036244},{"measure_id":1,"measure_name":"Deaths","location_id":58,"location_name":"Estonia","sex_id":2,"sex_name":"Female","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.3294458394252624,"upper":0.3753286076912417,"lower":0.28694828245786763},{"measure_id":1,"measure_name":"Deaths","location_id":58,"location_name":"Estonia","sex_id":3,"sex_name":"Both","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":1.0289266239495323,"upper":1.1584766467508971,"lower":0.9108785650118686},{"measure_id":1,"measure_name":"Deaths","location_id":83,"location_name":"Iceland","sex_id":1,"sex_name":"Male","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":1.6453349610814396,"upper":1.9720450501067082,"lower":1.3539891732870075},{"measure_id":1,"measure_name":"Deaths","location_id":83,"location_name":"Iceland","sex_id":2,"sex_name":"Female","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.9390145397574611,"upper":1.0614015114423119,"lower":0.8236909318542512},{"measure_id":1,"measure_name":"Deaths","location_id":83,"location_name":"Iceland","sex_id":3,"sex_name":"Both","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":1.2936587567049513,"upper":1.4617467651048455,"lower":1.12192803587556},{"measure_id":1,"measure_name":"Deaths","location_id":179,"location_name":"Ethiopia","sex_id":1,"sex_name":"Male","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1992,"val":0.23690051883533506,"upper":0.35795334331142303,"lower":0.08448984064191535},{"measure_id":1,"measure_name":"Deaths","location_id":179,"location_name":"Ethiopia","sex_id":2,"sex_name":"Female","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1992,"val":0.1361093288875428,"upper":0.20122327972272824,"lower":0.0645473361936783},{"measure_id":1,"measure_name":"Deaths","location_id":179,"location_name":"Ethiopia","sex_id":3,"sex_name":"Both","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1992,"val":0.18670603311047193,"upper":0.25454621350918094,"lower":0.07714122860196679},{"measure_id":1,"measure_name":"Deaths","location_id":435,"location_name":"South Sudan","sex_id":1,"sex_name":"Male","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.13649239779426126,"upper":0.29617331284112947,"lower":0.03297886132526967},{"measure_id":1,"measure_name":"Deaths","location_id":435,"location_name":"South Sudan","sex_id":2,"sex_name":"Female","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.02383311878794609,"upper":0.03962218940209911,"lower":0.010747618490300523},{"measure_id":1,"measure_name":"Deaths","location_id":435,"location_name":"South Sudan","sex_id":3,"sex_name":"Both","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.08364765943267895,"upper":0.171704986854593,"lower":0.0243379661934166},{"measure_id":1,"measure_name":"Deaths","location_id":59,"location_name":"Latvia","sex_id":1,"sex_name":"Male","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":1.4084754793712393,"upper":1.5916099966021162,"lower":1.2409455053062404},{"measure_id":1,"measure_name":"Deaths","location_id":59,"location_name":"Latvia","sex_id":2,"sex_name":"Female","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.29497986513032887,"upper":0.33132508731492794,"lower":0.26029976526210236},{"measure_id":1,"measure_name":"Deaths","location_id":59,"location_name":"Latvia","sex_id":3,"sex_name":"Both","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1990,"val":0.8136452649600315,"upper":0.9020641925856446,"lower":0.7293861762130845},{"measure_id":1,"measure_name":"Deaths","location_id":195,"location_name":"Namibia","sex_id":1,"sex_name":"Male","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1991,"val":1.790817176525065,"upper":2.280432368294814,"lower":1.292437519620907},{"measure_id":1,"measure_name":"Deaths","location_id":195,"location_name":"Namibia","sex_id":2,"sex_name":"Female","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1991,"val":0.5276998323359838,"upper":0.6591180662557725,"lower":0.42421137575377044},{"measure_id":1,"measure_name":"Deaths","location_id":195,"location_name":"Namibia","sex_id":3,"sex_name":"Both","age_id":22,"age_name":"All Ages","cause_id":562,"cause_name":"Opioid use disorders","metric_id":3,"metric_name":"Rate","year":1991,"val":1.1441951445006897,"upper":1.4163540175832094,"lower":0.8724621257038084}]
+
+class opiodDeathVisual {
+    constructor(root, rawData) {
+        this.root = root
+        this.unfilteredData = rawData
+
+        //reference to nodes
+        this.graph = root.querySelector('.graph')
+        this.controls = root.querySelector('.controls ul')
+        this.canvas = root.querySelector('#myChart')
+
+        //helpers
+        this.currActive = null
+        this.loaded = false
+
+        //data settings for each filter
+        this.filterBySexData = {
+            datasets: [{
+                data: [], //Male, Female, Both 
+                backgroundColor: ['rgb(54, 162, 235)', 'rgb(255, 99, 132)', 'rgb(255, 205, 86)'],
+            }],
+            labels: []
+        }
+
+        this.filterByYearData = {
+            labels: [],
+            datasets: [{
+                data: [], //this data has to be respective to the labels above
+                backgroundColor: 'rgb(70,205,207)',
+                label: 'Death By Year',
+                fill: false
+            }]
+        }
+
+        this.filterByCountryData = {
+            labels: [],
+            datasets: [{
+                data: [], //this data has to be respective to the labels above
+                backgroundColor: 'rgb(70,205,207)',
+                label: 'Death By Country',
+                fill: false
+            }]
+        }
+
+        //charSettings for each filter 
+        this.chartSettings = {
+            'sex': {
+                type: 'doughnut',
+                data: this.filterBySexData,
+                options: {
+                    title: {
+                        display: true,
+                        text: 'Death Sorted by Sex',
+                        fontSize: 18
+                    },
+                    maintainAspectRatio: false,
+                }
+
+            },
+            'year': {
+                type: 'line',
+                data: this.filterByYearData,
+                options: {
+                    showLines: false, //  dont draw line to boost performance
+
+                    legend: false,
+                    title: {
+                        display: true,
+                        text: 'Death Sorted by Year',
+                        fontSize: 18
+                    },
+                    maintainAspectRatio: false,
+                }
+            },
+            'country': {
+                type: 'horizontalBar',
+                data: this.filterByCountryData,
+                options: {
+                    scales: {
+                        xAxes: [{
+                            ticks: {
+                                beginAtZero: true,
+                                stepSize: 10
+                            }
+                        }],
+                    },
+                    legend: false,
+                    title: {
+                        display: true,
+                        text: 'Death Sorted by Country',
+                        fontSize: 18
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false,
+                }
+
+            }
+        }
+
+        //attach all eventListeners
+        this.controls.addEventListener('click', (e) => {
+            if (e.target.matches('li')) {
+                this.changeGraph(e.target)
+            }
+        })
+
+        //initial calls to filter the data and set up the view
+        this.filterData()
+        this.changeGraph(root.querySelector('.controls .active')) //get the node with '.active' class and pass in for first view
+    }
+
+    //loop over rawData and and pass into filterBySexData, filterByYearData and filterByCountryData obeying the format chartJS requirements
+    filterData() {
+
+        //I use map instead of object because Map has a easier manipulate
+        let unfilteredSexData = new Map()
+        let unfilteredYearData = new Map()
+        let unfilteredCountryData = new Map()
+
+        this.unfilteredData.forEach((record) => {
+            unfilteredSexData.set(record['sex_name'], unfilteredSexData.get(record['sex_name']) + 1 || 1)
+            unfilteredYearData.set(record['year'], unfilteredYearData.get(record['year']) + 1 || 1)
+            unfilteredCountryData.set(record['location_name'], unfilteredCountryData.get(record['location_name']) + 1 || 1)
+
+        })
+
+        let sortedYearData = new Map([...unfilteredYearData.entries()].sort((a, b) => a[0] - b[0]))
+        let sortedCountryData = new Map([...unfilteredCountryData.entries()].sort())
+
+        this.populateData(unfilteredSexData, this.filterBySexData)
+        this.populateData(sortedYearData, this.filterByYearData)
+        this.populateData(sortedCountryData, this.filterByCountryData)
+
+        //cleaning up temporary variables
+        unfilteredSexData = null
+        unfilteredYearData = null
+        unfilteredCountryData = null
+        sortedYearData = null
+        sortedCountryData = null
+    }
+
+    populateData(unfiltered, filtered) {
+        unfiltered.forEach((val, key) => {
+            filtered.labels.push(key)
+            filtered.datasets[0].data.push(val)
+        })
+    }
+
+
+    changeGraph(node) {
+        if (node != this.currActive) {
+
+            this.currActive = node
+
+            //remove the current canvas element and create a new one
+            this.graph.removeChild(this.canvas)
+            let canvas = document.createElement("canvas")
+            canvas.setAttribute('id', 'myChart')
+            this.graph.appendChild(canvas)
+            this.canvas = canvas
+
+            let filterBy = node.getAttribute('data-filter')
+
+            let containerHeight = 'auto' //to scale the container according to the size of graph
+            if (filterBy === 'country') {
+                containerHeight = this.filterByCountryData.labels.length * 40
+            }
+
+            this.graph.setAttribute('style', `height: ${containerHeight}px`)
+            new Chart(canvas, this.chartSettings[filterBy])
+            this.updateUI()
+        }
+    }
+
+    updateUI() {
+        this.root.querySelector('.active').classList.remove('active')
+        this.currActive.classList.add('active')
+    }
+}
+
+(function() { //iife to avoid globals
+    let root = document.getElementById('opiodDeathVisual')
+    new opiodDeathVisual(root, rawData)
+})();
